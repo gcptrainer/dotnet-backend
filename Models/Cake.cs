cb298efe06fb3fafa474e253f5dbc8aacea7cf3c
gcptrainer <98963708+gcptrainer@users.noreply.github.com>
2026-01-31 13:07:41 +0530

    Create Cake.cs

---
+++ Models/Cake.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetapp.Models
+{
+    public class Cake
+    {
+        //MS
+        [Key]
+        public int CakeId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string CakeImage { get; set; }
+
+    }
+}
